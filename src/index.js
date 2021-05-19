8a0a9c97ad85ba54bb4650e5f03ab6acc276bb93
cheedinmar <cheedinmarblessing@gmail.com>
2021-05-19 23:55:31 +0100

    JSX Css, Props, props destructuring

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import ReactDom from 'react-dom'
 
+//CSS
+import './index.css'
+
 
 //JSX RULES
 //return single element
@@ -13,21 +16,48 @@ import ReactDom from 'react-dom'
 
 //NESTED COMPONENTS, REACT TOOLS
 
-function Greeting(){
-   return (
-     <div>
-      <Person />
-      <Message/>
-      
-   </div>
-   );
+//SETUP VARS
+
+const firstBook ={
+        title:'Zero Fail: The Rise and Fall of the Secret Service',
+        author:' Amelia Hempwirth',
+        img: 'https://images-na.ssl-images-amazon.com/images/I/81cNkDeurKL._AC_UL200_SR200,200_.jpg'
+
+}
+const secondBook ={
+        title:'The Four Agreements: A Practical Guide to Personal Freedom (A Toltec Wisdom Book',
+        author:' Don Miguel Ruiz',
+        img: 'https://images-na.ssl-images-amazon.com/images/I/91p5b0UgbKL._AC_UL200_SR200,200_.jpg'
+
 }
 
-const Person = () => <h2>John Doe</h2>;
-const Message =() => {
-  return <h4>This is my message</h4>;
-};
+
+function BookList(){
+    return( 
+    <section className='booklist'>  
+        <Book  title={firstBook.title} author={firstBook.author} img={firstBook.img}/>
+        <Book img={secondBook.img} title={secondBook.title} author={secondBook.author}  />
+        <Book/>
+        <Book/>
+
+    </section>
+    );
+}
+
+function Book(title, img, author){
+    //object destructuring
+    // const {title, img, author} = props;
+    console.log()
+    return (<article className='book'>
+        <img src={img} alt=''/>
+        <h1>{title} </h1>
+        <h4 style={{color:'#617d98', fontSize: '0.75rem', marginTop: '0.25rem' }}>{author}</h4>
+    </article>);
+}
+
+
+
 
 //render method takes two things,
 //what to render and where to render
-ReactDom.render(<Greeting/> , document.getElementById('root '))
+ReactDom.render(<BookList /> , document.getElementById('root'));
